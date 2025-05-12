74948805d9b17cf187e328f77971f6a7b45641fe
naimislamm <naimmislam@gmail.com>
2025-05-12 13:01:43 +0600

    added country in a parent div which i visited

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setvisitedCountries] = useState([]);
+
+  const handleVisitedCountry = (country) => {
+    console.log(country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setvisitedCountries(newVisitedCountries);
+  };
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -12,9 +20,24 @@ const Countries = () => {
   return (
     <div>
       <h3>Countries:{countries.length}</h3>
-      {countries.map((country) => (
-        <Country key={country.cca3} country={country}></Country>
-      ))}
+      <div>
+        <h2>Visited Country:{visitedCountries.length}</h2>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country
+            key={country.cca3}
+            handleVisitedCountry={handleVisitedCountry}
+            country={country}
+          ></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,26 @@
-const Country = ({ country }) => {
-  const { name, flags } = country;
-  console.log(country);
+const Country = ({ country, handleVisitedCountry }) => {
+  const { name, flags, population, cca3 } = country;
+  const [visitingCountry, setVisitingCountry] = useState(false);
+  const handleVisited = () => {
+    setVisitingCountry(!visitingCountry);
+  };
+
   return (
-    <div className="country">
+    <div className={`country ${visitingCountry ? "visited" : ""}`}>
       <h3>Name:{name.common}</h3>
       <img src={flags.png} alt="flag-image" />
+      <p>Population:{population}</p>
+      <p>Code:{cca3}</p>
+      <button onClick={handleVisited}>
+        {visitingCountry ? "Visited" : "visit"}
+      </button>
+      {visitingCountry ? "visited" : ""}
+      <button onClick={() => handleVisitedCountry(country)}>
+        Add Visited Country
+      </button>
     </div>
   );
 };
+import { useState } from "react";
 import "./Country.css";
 export default Country;
